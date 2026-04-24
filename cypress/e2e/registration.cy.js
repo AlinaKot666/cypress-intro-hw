d4eac6a1baf5636ae8e7b4554b8c452e1e10ac54
AlinaKot666 <alinakotliarenko666@gmail.com>
2026-04-24 14:45:41 +0200

    final version with login command

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,30 +1,11 @@
 describe('Registration', () => {
 
   beforeEach(() => {
-    cy.visit('https://qauto.forstudy.space/');
-    cy.contains('Registration').click();
-  });
+  cy.visit('https://guest:welcome2qauto@qauto.forstudy.space/');
 
-  it('Should show errors for empty fields', () => {
-    cy.contains('Register').click();
-
-    cy.contains('Name required').should('be.visible');
-    cy.contains('Last name required').should('be.visible');
-    cy.contains('Email required').should('be.visible');
-    cy.contains('Password required').should('be.visible');
-  });
-
-  it('Should validate email format', () => {
-    cy.get('#signupEmail').type('invalidEmail');
-    cy.contains('Email is incorrect').should('be.visible');
-  });
-
-  it('Should show error if passwords do not match', () => {
-    cy.get('#signupPassword').type('Password123');
-    cy.get('#signupRepeatPassword').type('Password124');
-
-    cy.contains('Passwords do not match').should('be.visible');
-  });
+  cy.contains('Sign In').click();
+  cy.contains('Registration').click();
+});
 
   it('Should register successfully', () => {
     const email = `test${Date.now()}@mail.com`;

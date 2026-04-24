9aaad82622870cb70b87524bb44f3de7619641a1
AlinaKot666 <alinakotliarenko666@gmail.com>
2026-04-24 14:19:34 +0200

    add registration tests

---
+++ cypress/e2e/registration.cy.js
@@ -0,0 +1,31 @@
+describe('Registration', () => {
+
+  beforeEach(() => {
+    cy.visit('https://qauto.forstudy.space/');
+    cy.contains('Registration').click();
+  });
+
+  it('Should show errors for empty fields', () => {
+    cy.contains('Register').click();
+
+    cy.contains('Name required').should('be.visible');
+    cy.contains('Last name required').should('be.visible');
+    cy.contains('Email required').should('be.visible');
+    cy.contains('Password required').should('be.visible');
+  });
+
+  it('Should register successfully', () => {
+    const email = `test${Date.now()}@mail.com`;
+
+    cy.get('#signupName').type('John');
+    cy.get('#signupLastName').type('Doe');
+    cy.get('#signupEmail').type(email);
+    cy.get('#signupPassword').type('Password123');
+    cy.get('#signupRepeatPassword').type('Password123');
+
+    cy.contains('Register').click();
+
+    cy.url().should('include', 'garage');
+  });
+
+});

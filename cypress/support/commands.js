b3fb2302a055661696ee3d6da2ff921ef57e30be
AlinaKot666 <alinakotliarenko666@gmail.com>
2026-06-10 21:06:59 +0200

    Fix expense date for CI

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,23 +1,21 @@
 Cypress.Commands.add('createExpense', (sid, carId) => {
+  const tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+
+  const formattedDate = tomorrow.toISOString().split('T')[0];
 
   cy.request({
-
     method: 'POST',
-
     url: 'https://qauto.forstudy.space/api/expenses',
-
     headers: {
       Cookie: sid
     },
-
     body: {
       carId: carId,
-      reportedAt: '2026-05-07',
+      expenseDate: formattedDate,
       mileage: 150,
       liters: 20,
-      totalCost: 100
+      pricePerLitre: 1
     }
-
-  })
-
-})
+  });
+});
